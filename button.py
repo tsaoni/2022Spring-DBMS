15930e5f7b88ef8dff158643072b4c447e074734
minimelonling <sillosougri270415@gmail.com>
2022-06-03 16:31:37 +0800

    button insert in company success

--- button.py
+++ button.py
@@ -1,4 +1,5 @@
 from tkinter import *
+import sqlite3
 import sys
 import os
 
@@ -9,46 +10,54 @@ def restart_program(root):
 def op_interface(root, op, q_elements):
 	for e in q_elements:
 		e.destroy()
-	otitle = Label(text="Please select the conditions:", font=("Arial", 10))
+	# otitle = Label(text="Please select the conditions:", font=("Arial", 10))
 
 	if op == "SELECT-FROM-WHERE":
-		pass
+		SELECT_FROM_WHERE()
 	elif op == "DELETE":
-		pass
+		DELETE()
 	elif op == "INSERT":
-		pass
+		INSERT(root, 0)
 	elif op == "UPDATE":
-		pass
+		UPDATE()
 	elif op == "IN":
-		pass
+		IN()
 	elif op == "NOT IN":
-		pass
+		NOT_IN()
 	elif op == "EXISTS":
-		pass
+		EXISTS()
 	elif op == "COUNT":
-		pass
+		COUNT()
 	elif op == "SUM":
-		pass
+		SUM()
 	elif op == "MAX":
-		pass
+		MAX()
 	elif op == "MIN":
-		pass
+		MIN()
 	elif op == "AVG":
-		pass
+		AVG()
 	else: # op == "HAVING"
-		pass
+		HAVING()
 
-	ok_button = Button(root, text="ok", command=show_result)
+	
 	back_button = Button(root, text="restart", command=lambda: restart_program(root))
 	
-	otitle.grid(row=0, column=0)
-	ok_button.grid(row=1, column=0)
-	back_button.grid(row=2, column=0)
+	# otitle.grid(row=0, column=0)
+	back_button.grid(row=11, column=0)
 
 	return
-
+"""
 def show_result():
-	pass
+	# database
+	conn = sqlite3.connect('database.db')
+	c = conn.cursor()
+	# execute sql script
+	c.execute("INSERT INTO company (COMPANY_ID, COMPANY_NAME, IS_INTERNATIONAL) VALUES(?, ?, ?)",
+			  [id_entry.get(), name_entry.get(), value.get()])
+	print(c.execute("SELECT * FROM COMPANY").fetchall())
+	conn.commit()
+	conn.close()
+"""
 
 def button_page(root):
 	# button page element declaration
@@ -70,3 +79,147 @@ def button_page(root):
 	back_button.grid(row=3, column=0)
 
 	op_value.set("SELECT-FROM-WHERE") # default value
+
+# SQL operation
+def SELECT_FROM_WHERE():
+	pass
+
+def DELETE():
+	pass
+	
+
+def INSERT(root, page, ele = None, table = None):
+	# choose the table to insert
+	if page == 0:
+		title = Label(text="Please choose a table to insert:", font=("Arial", 10))
+		value = StringVar(root)
+		menu = OptionMenu(root, value, "COMPANY", 
+					   "DEVELOPER", "APP", "PLATFORM", 
+					   "CLIENT", "TRADE", "REPORT_BUG")
+		value.set("COMPANY") # default value
+		ok_button = Button(root, text="next", command=lambda: INSERT(root, 1, elements, value.get()))
+		elements = [title, menu, ok_button]
+
+		title.grid(row=0, column=0)
+		menu.grid(row=1, column=0)
+		ok_button.grid(row=2, column=0)
+
+	# insert conditions
+	elif page == 1:
+		# delete elements from last page
+		for e in ele:
+			e.destroy()
+		f = None
+		# insert elements in new page
+		if table == "COMPANY":
+			company(root, "INSERT", 0)
+			f = lambda: company(root, "INSERT", 1)
+		elif table == "DEVELOPER":
+			developer("INSERT", 0)
+			f = lambda: developer("INSERT", 1)
+		elif table == "APP":
+			APP("INSERT", 0)
+			f = lambda: APP("INSERT", 1)
+		elif table == "PLATFORM":
+			platform("INSERT", 0)
+			f = lambda: platform("INSERT", 1)
+		elif table == "CLIENT":
+			client("INSERT", 0)
+			f = lambda: client("INSERT", 1)
+		elif table == "TRADE":
+			trade("INSERT", 0)
+			f = lambda: trade("INSERT", 1)
+		else: # table == "REPORT_BUG"
+			report_bug("INSERT", 0)
+			f = lambda: report_bug("INSERT", 1)
+
+		ok_button = Button(root, text="ok", command= f)
+		ok_button.grid(row=10, column=0)
+
+
+
+def UPDATE():
+	pass
+
+def IN():
+	pass
+
+def NOT_IN():
+	pass
+
+def EXISTS():
+	pass
+
+def COUNT():
+	pass
+
+def SUM():
+	pass
+
+def MAX():
+	pass
+
+def MIN():
+	pass
+
+def AVG():
+	pass
+
+def HAVING():
+	pass
+
+# table operation
+company_element = []
+def company(root, op, page):
+	global company_element
+	if op == "INSERT":
+		if page == 0:
+			# GUI
+			id_label = Label(text="Company ID", font=("Arial", 10))
+			name_label = Label(text="Company Name", font=("Arial", 10))
+			inter_label = Label(text="is international?", font=("Arial", 10))
+			id_entry = Entry(root, width=30)
+			name_entry = Entry(root, width=30)
+			company_element.append(id_entry)
+			company_element.append(name_entry)
+			value = StringVar(root)
+			company_element.append(value)
+			is_international = OptionMenu(root, value, "yes", "no")
+			value.set("yes") # default value
+
+			id_label.grid(row=0, column=0)
+			id_entry.grid(row=1, column=0)
+			name_label.grid(row=2, column=0)
+			name_entry.grid(row=3, column=0)
+			inter_label.grid(row=4, column=0)
+			is_international.grid(row=5, column=0)
+		if page == 1:
+			# database
+			conn = sqlite3.connect('database.db')
+			c = conn.cursor()
+			# execute sql script
+			c.execute("INSERT INTO company (COMPANY_ID, COMPANY_NAME, IS_INTERNATIONAL) VALUES(?, ?, ?)",
+					 [company_element[0].get(), company_element[1].get(), company_element[2].get()])
+			print(c.execute("SELECT * FROM COMPANY").fetchall())
+			conn.commit()
+			conn.close()
+			company_element[0].delete(0, END)
+			company_element[1].delete(0, END)
+
+def developer(op):
+	pass
+
+def APP(op):
+	pass
+
+def platform(op):
+	pass
+
+def client(op):
+	pass
+
+def trade(op):
+	pass
+
+def report_bug(op):
+	pass
