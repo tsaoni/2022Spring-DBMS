580cce4a57b078c5e0df95b3aba100ade7a6fa3e
minimelonling <sillosougri270415@gmail.com>
2022-06-03 21:25:52 +0800

    APP sum success

--- button.py
+++ button.py
@@ -19,7 +19,7 @@ def op_interface(root, op, q_elements):
 	elif op == "INSERT":
 		INSERT(root, 0)
 	elif op == "UPDATE":
-		UPDATE()
+		UPDATE(root, 0)
 	elif op == "IN":
 		IN()
 	elif op == "NOT IN":
@@ -29,7 +29,7 @@ def op_interface(root, op, q_elements):
 	elif op == "COUNT":
 		COUNT()
 	elif op == "SUM":
-		SUM()
+		SUM(root, 0)
 	elif op == "MAX":
 		MAX()
 	elif op == "MIN":
@@ -85,7 +85,7 @@ def SELECT_FROM_WHERE():
 	pass
 
 def DELETE(root, page, ele = None, table = None):
-	# choose the table to insert
+	# choose the table to delete
 	if page == 0:
 		title = Label(text="Please choose a table to delete:", font=("Arial", 10))
 		value = StringVar(root)
@@ -100,13 +100,13 @@ def DELETE(root, page, ele = None, table = None):
 		menu.grid(row=1, column=0)
 		ok_button.grid(row=2, column=0)
 
-	# insert conditions
+	# delete conditions
 	elif page == 1:
 		# delete elements from last page
 		for e in ele:
 			e.destroy()
 		f = None
-		# insert elements in new page
+		# delete elements in new page
 		if table == "COMPANY":
 			company(root, "DELETE", 0)
 			f = lambda: company(root, "DELETE", 1)
@@ -184,8 +184,53 @@ def INSERT(root, page, ele = None, table = None):
 
 
 
-def UPDATE():
-	pass
+def UPDATE(root, page, ele = None, table = None):
+	# choose the table to update
+	if page == 0:
+		title = Label(text="Please choose a table to update:", font=("Arial", 10))
+		value = StringVar(root)
+		menu = OptionMenu(root, value, "COMPANY", 
+					   "DEVELOPER", "APP", "PLATFORM", 
+					   "CLIENT", "TRADE", "REPORT_BUG")
+		value.set("COMPANY") # default value
+		ok_button = Button(root, text="next", command=lambda: UPDATE(root, 1, elements, value.get()))
+		elements = [title, menu, ok_button]
+
+		title.grid(row=0, column=0)
+		menu.grid(row=1, column=0)
+		ok_button.grid(row=2, column=0)
+
+	# update conditions
+	elif page == 1:
+		# delete elements from last page
+		for e in ele:
+			e.destroy()
+		f = None
+		# update elements in new page
+		if table == "COMPANY":
+			company(root, "UPDATE", 0)
+			f = lambda: company(root, "UPDATE", 1)
+		elif table == "DEVELOPER":
+			developer("UPDATE", 0)
+			f = lambda: developer("UPDATE", 1)
+		elif table == "APP":
+			APP(root, "UPDATE", 0)
+			f = lambda: APP(root, "UPDATE", 1)
+		elif table == "PLATFORM":
+			platform("UPDATE", 0)
+			f = lambda: platform("UPDATE", 1)
+		elif table == "CLIENT":
+			client("UPDATE", 0)
+			f = lambda: client("UPDATE", 1)
+		elif table == "TRADE":
+			trade("UPDATE", 0)
+			f = lambda: trade("UPDATE", 1)
+		else: # table == "REPORT_BUG"
+			report_bug("UPDATE", 0)
+			f = lambda: report_bug("UPDATE", 1)
+
+		ok_button = Button(root, text="ok", command= f)
+		ok_button.grid(row=10, column=0)
 
 def IN():
 	pass
@@ -199,8 +244,53 @@ def EXISTS():
 def COUNT():
 	pass
 
-def SUM():
-	pass
+def SUM(root, page, ele = None, table = None):
+	# choose the table to do sum
+	if page == 0:
+		title = Label(text="Please choose a table to do sum:", font=("Arial", 10))
+		value = StringVar(root)
+		menu = OptionMenu(root, value, "COMPANY", 
+					   "DEVELOPER", "APP", "PLATFORM", 
+					   "CLIENT", "TRADE", "REPORT_BUG")
+		value.set("COMPANY") # default value
+		ok_button = Button(root, text="next", command=lambda: SUM(root, 1, elements, value.get()))
+		elements = [title, menu, ok_button]
+
+		title.grid(row=0, column=0)
+		menu.grid(row=1, column=0)
+		ok_button.grid(row=2, column=0)
+
+	# do sum conditions
+	elif page == 1:
+		# delete elements from last page
+		for e in ele:
+			e.destroy()
+		f = None
+		# do sum in new page
+		if table == "COMPANY":
+			company(root, "SUM", 0)
+			f = lambda: company(root, "SUM", 1)
+		elif table == "DEVELOPER":
+			developer("SUM", 0)
+			f = lambda: developer("SUM", 1)
+		elif table == "APP":
+			APP(root, "SUM", 0)
+			f = lambda: APP(root, "SUM", 1)
+		elif table == "PLATFORM":
+			platform("SUM", 0)
+			f = lambda: platform("SUM", 1)
+		elif table == "CLIENT":
+			client("SUM", 0)
+			f = lambda: client("SUM", 1)
+		elif table == "TRADE":
+			trade("SUM", 0)
+			f = lambda: trade("SUM", 1)
+		else: # table == "REPORT_BUG"
+			report_bug("SUM", 0)
+			f = lambda: report_bug("SUM", 1)
+
+		ok_button = Button(root, text="ok", command= f)
+		ok_button.grid(row=10, column=0)
 
 def MAX():
 	pass
@@ -275,6 +365,35 @@ def company(root, op, page):
 			conn.close()
 			company_element[0].delete(0, END)
 
+	if op == "UPDATE":
+		if page == 0:
+			# GUI
+			company_element = []
+			id_label = Label(text="Please type Company ID to update", font=("Arial", 10))
+			name_label = Label(text="Please type the new Company Name", font=("Arial", 10))
+			id_entry = Entry(root, width=30)
+			name_entry = Entry(root, width=30)
+			company_element.append(id_entry)
+			company_element.append(name_entry)
+
+			id_label.grid(row=0, column=0)
+			id_entry.grid(row=1, column=0)
+			name_label.grid(row=2, column=0)
+			name_entry.grid(row=3, column=0)
+			
+		if page == 1:
+			# database
+			conn = sqlite3.connect('database.db')
+			c = conn.cursor()
+			# execute sql script
+			c.execute("UPDATE COMPANY SET COMPANY_NAME = ? WHERE COMPANY_ID = ?",
+					 [company_element[1].get(), company_element[0].get()])
+			print(c.execute("SELECT * FROM COMPANY").fetchall())
+			conn.commit()
+			conn.close()
+			company_element[0].delete(0, END)
+			company_element[1].delete(0, END)
+
 def developer(op):
 	pass
 
@@ -313,7 +432,7 @@ def APP(root, op, page):
 			c = conn.cursor()
 			# execute sql script
 			c.execute("INSERT INTO APP (APP_ID, INSTALL_NUM, COST, RELEASE_ID) VALUES(?, ?, ?, ?)",
-					 [APP_element[0].get(), int(APP_element[1].get()), int(APP_element[2].get()), APP_element[3].get()])
+					 [APP_element[0].get(), int(APP_element[2].get()), int(APP_element[1].get()), APP_element[3].get()])
 			print(c.execute("SELECT * FROM APP").fetchall())
 			conn.commit()
 			conn.close()
@@ -340,6 +459,62 @@ def APP(root, op, page):
 			c.execute("DELETE FROM APP WHERE APP_ID = ?",
 					 [APP_element[0].get()])
 			print(c.execute("SELECT * FROM APP").fetchall())
+			conn.commit()
+			conn.close()
+			APP_element[0].delete(0, END)
+
+	if op == "UPDATE":
+		if page == 0:
+			# GUI
+			APP_element = []
+			id_label = Label(text="Please type APP ID to update", font=("Arial", 10))
+			cost_label = Label(text="Please type the new APP Cost", font=("Arial", 10))
+			company_label = Label(text="Please type the new APP company ID", font=("Arial", 10))
+			id_entry = Entry(root, width=30)
+			cost_entry = Entry(root, width=30)
+			company_entry = Entry(root, width=30)
+			APP_element.append(id_entry)
+			APP_element.append(cost_entry)
+			APP_element.append(company_entry)
+
+			id_label.grid(row=0, column=0)
+			id_entry.grid(row=1, column=0)
+			cost_label.grid(row=2, column=0)
+			cost_entry.grid(row=3, column=0)
+			company_label.grid(row=4, column=0)
+			company_entry.grid(row=5, column=0)
+
+		if page == 1:
+			# database
+			conn = sqlite3.connect('database.db')
+			c = conn.cursor()
+			# execute sql script
+			c.execute("UPDATE APP SET COST = ?, RELEASE_ID = ? WHERE APP_ID = ?",
+					 [int(APP_element[1].get()), APP_element[2].get(), APP_element[0].get()])
+			print(c.execute("SELECT * FROM APP").fetchall())
+			conn.commit()
+			conn.close()
+			APP_element[0].delete(0, END)
+			APP_element[1].delete(0, END)
+			APP_element[2].delete(0, END)
+
+	if op == "SUM":
+		if page == 0:
+			# GUI
+			APP_element = []
+			company_label = Label(text="Type the company ID to get sum of APP install number", font=("Arial", 10))
+			company_entry = Entry(root, width=30)
+			APP_element.append(company_entry)
+			company_label.grid(row=0, column=0)
+			company_entry.grid(row=1, column=0)
+		if page == 1:
+			# database
+			conn = sqlite3.connect('database.db')
+			c = conn.cursor()
+			# execute sql script
+			print(c.execute("SELECT RELEASE_ID, SUM(INSTALL_NUM) AS SUM_TABLE FROM APP WHERE RELEASE_ID = ?",
+					 [APP_element[0].get()]).fetchall())
+			# print(c.execute("SELECT * FROM APP").fetchall())
 			conn.commit()
 			conn.close()
 			APP_element[0].delete(0, END)
